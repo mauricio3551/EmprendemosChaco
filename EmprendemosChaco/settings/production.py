37c83a583c0e3c81fcfe7c7b65f781cc184cf87c
mauricio gutierrez <mauriciogutierrez3551@gmail.com>
2024-09-22 14:03:34 -0300

    Esqueleto del proyecto

---
+++ EmprendemosChaco/settings/production.py
@@ -0,0 +1,22 @@
+from .base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-9!03f=nox@ry&tz%gk+p(zi1er0alpy^318qrz8317&v8iga%)'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
