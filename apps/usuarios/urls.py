ffe80dff20a4bbcc90b5c6e9983072253073fa35
mauricio gutierrez <mauriciogutierrez3551@gmail.com>
2024-10-04 19:28:01 -0300

    Se agrega la base.html completa

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import profile_list
+#from .views import profile_list
 
-urlpatterns = [
-    path('', profile_list, name='profile_list'),
-]
+#urlpatterns = [
+#    path('', profile_list, name='profile_list'),
+#]
