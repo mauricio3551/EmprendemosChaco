d1b2c98bc33d2fd139123bf611c214bfb421eb4e
mauricio gutierrez <mauriciogutierrez3551@gmail.com>
2024-10-11 22:14:57 -0300

    Se modifico los datos al crear post

--- apps/post/forms.py
+++ apps/post/forms.py
@@ -6,4 +6,4 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'thumbnail', 'category']
-        labels = {'title': 'Ingrese un titulo'}
+        labels = {'title': 'Ingrese un titulo', 'content': 'Informacion', 'thumbnail': 'imagen', 'category': 'categoria'}
