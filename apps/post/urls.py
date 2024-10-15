58070706b8530475d80a04bcc558f9ab62485cc9
mauricio gutierrez <mauriciogutierrez3551@gmail.com>
2024-10-15 15:37:55 -0300

    Se agrega la plantilla de post creado con exito

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from apps.post.views import *
 
 app_name = "post"
 
 urlpatterns = [
     #post
-    path('post/crear', PostCrearView.as_view(), name='crearPost'),
+    path('post/crear', PostCrearView.as_view(success_url = reverse_lazy('post:postCreado')), name='crearPost'),
+    path('post/creado', PostCrearView.as_view(template_name = 'post/postCreationSuccess.html'), name = 'postCreado'),
     path('post/<int:pk>', PostMostrarView.as_view(), name='mostrarPost'),
 
     #comentario
