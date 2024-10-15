2ce57957d6d996108a62284b78263dbff203bed3
mauricio gutierrez <mauriciogutierrez3551@gmail.com>
2024-10-14 23:58:34 -0300

    se agrego los comentarios y la opcion de verlos en el post

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -4,6 +4,11 @@ from apps.post.views import *
 app_name = "post"
 
 urlpatterns = [
+    #post
     path('post/crear', PostCrearView.as_view(), name='crearPost'),
     path('post/<int:pk>', PostMostrarView.as_view(), name='mostrarPost'),
+
+    #comentario
+    path('post/<int:pk>/comentario', PostComentarioView.as_view(), name = 'nuevoComentario'),
+    path('post/listar/comentario', postComentarios, name='listarComentarios')
 ]
--- apps/post/views.py
+++ apps/post/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, DetailView
-from apps.post.forms import PostForm
-from .models  import Post
+from apps.post.forms import PostForm, CommentForm
+from .models  import Post, Comment
 from django.urls.base import reverse_lazy
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from django.conf import settings
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import Count
 
 
 class PostCrearView(LoginRequiredMixin, CreateView):
@@ -28,3 +31,24 @@ class PostMostrarView(DetailView):
     def getContextData(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+class PostComentarioView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'post/postCommentForm.html'
+    success_url = reverse_lazy('inicio')
+    login_url = settings.LOGIN_URL
+
+    def form_valid(self, form):
+        new = form.save(commit = False)
+        new.post_id = self.kwargs['pk']
+        new.user = self.request.user
+        new.save()
+
+        return HttpResponseRedirect(reverse('post:mostrarPost', args = [str(new.post_id)]))
+
+def postComentarios(request):
+    opcion = request.GET.get('select')
+    posts = Post.objects.annotate(num_comments=Count('commentsPost')).order_by('-num_comments')
+
+    return render(request,'post/postList.html', {'posts':posts})
