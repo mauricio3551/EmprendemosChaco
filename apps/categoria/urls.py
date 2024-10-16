57b5fc22f98d5112dae3632456b074e86a9771e9
lautaroleario26 <lautaro.lea.rios@gmail.com>
2024-10-15 21:47:05 -0300

    commit_de_prueba

--- apps/categoria/urls.py
+++ apps/categoria/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, reverse_lazy
-from .views import CategoriaCrearView
+from .views import CategoriaCrearView, listar_posts_por_categoria  # Importa la vista para listar los posts por categoría
 from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = "categoria"
 
 urlpatterns = [
-    # url para agregar categorias
-    path('categoria/agregar', CategoriaCrearView.as_view(success_url = reverse_lazy('categoria:categoriaCreada')), name='crearCategoria'),
-    path('categoria/creada/', CategoriaCrearView.as_view(template_name = 'categorias/categoriaCreada.html'), name='categoriaCreada'),
+    # URL para agregar categorías
+    path('categoria/agregar', CategoriaCrearView.as_view(success_url=reverse_lazy('categoria:categoriaCreada')), name='crearCategoria'),
+    path('categoria/creada/', CategoriaCrearView.as_view(template_name='categorias/categoriaCreada.html'), name='categoriaCreada'),
+
+    # URL para listar los posts por categoría
+    path('categoria/<int:categoria_id>/posts/', listar_posts_por_categoria, name='posts_por_categoria'),
 ]
--- apps/categoria/views.py
+++ apps/categoria/views.py
@@ -1,22 +1,26 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import CategoriaForm
 from .models import Categoria
 from django.urls.base import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView
-
-
+from apps.post.models import Post  # Importar el modelo de Post para listar los posts por categoría
 
 class CategoriaCrearView(LoginRequiredMixin, CreateView):
     model = Categoria
     form_class = CategoriaForm
     template_name = 'categorias/categoriaForm.html'
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categorias'] = Categoria.objects.all()  
         return context
+
+# Vista para listar los posts por categoría
+def listar_posts_por_categoria(request, categoria_id):
+    categoria = get_object_or_404(Categoria, id=categoria_id)
+    posts = Post.objects.filter(category=categoria)
+    return render(request, 'categoria/post_list_por_categoria.html', {'posts': posts, 'categoria': categoria})
+
 
 
