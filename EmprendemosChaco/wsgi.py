707b030fef0174ba123181e94eb6ad78e7595f4c
mauricio gutierrez <mauriciogutierrez3551@gmail.com>
2024-10-19 17:18:55 -0300

    odio python anywhere

--- EmprendemosChaco/wsgi.py
+++ EmprendemosChaco/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EmprendemosChaco.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EmprendemosChaco.settings.production')
 
 application = get_wsgi_application()
